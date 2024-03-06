0fb55e4e6261695ef46adeedb96ec648e887c805
CristianRdz <20213tn086@utez.edu.mx>
2024-03-05 18:45:24 -0600

    ADD docker compose

--- src/main/java/mx/edu/utez/sda/springmvc/controller/ControlPeliculas.java
+++ src/main/java/mx/edu/utez/sda/springmvc/controller/ControlPeliculas.java
@@ -6,10 +6,13 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import java.util.List;
 @Controller
 @RequestMapping("/")
 public class ControlPeliculas {
+
+    final String Mensaje = "Bienvenido a la página de películas";
+    //final String  MSJ ="";
 
     @RequestMapping(value ="/",method = RequestMethod.GET)
     @Secured({"ROLE_RECE", "ROLE_NINIO", "ROLE_ADULTO"})
